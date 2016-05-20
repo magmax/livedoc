7b171704c270f73ca1bdeac655e2669644d691ed
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2016-05-20 07:12:15 +0200

    fix coverage

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,16 +1,16 @@
 import unittest
-import livedoc
+from livedoc import LiveDoc
 
 
 class BasicUsage(unittest.TestCase):
     def test_variable_assignment_and_echo(self):
-        sut = livedoc.LiveDoc('assign [Jane Smith](- "c:set=#name") and return [Jane Smith](- "?=#name")')
+        sut = LiveDoc('assign [Jane Smith](- "c:set=#name") and return [Jane Smith](- "?=#name")')
         assert 'success' in sut.render()
 
     def test_variable_assignment_and_echo_with_shortcut(self):
-        sut = livedoc.LiveDoc('assign [Jane Smith](- "#name") and return [Jane Smith](- "?=#name")')
+        sut = LiveDoc('assign [Jane Smith](- "#name") and return [Jane Smith](- "?=#name")')
         assert 'success' in sut.render()
 
     def test_variable_assignment_and_failure(self):
-        sut = livedoc.LiveDoc('assign [Jane Smith](- "c:set=#name") and not return [Jane Mully](- "?=#name")')
+        sut = LiveDoc('assign [Jane Smith](- "c:set=#name") and not return [Jane Mully](- "?=#name")')
         assert 'failure' in sut.render()
