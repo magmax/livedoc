7c0971b8d4edfa1d2353ac85df2bbb775224f6a6
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2016-06-07 08:15:01 +0200

    again

--- tests/test_expression_factory.py
+++ tests/test_expression_factory.py
@@ -9,7 +9,7 @@ class ExpressionFactoryTest(unittest.TestCase):
     def test_simple_print(self):
         assert type(expression_factory("OUT = 3")) == Print
 
-    def test_simple_comparation(self):
+    def test_simple_comparison(self):
         assert type(expression_factory("4 == 3")) == Comparison
 
     def test_simple_call(self):
